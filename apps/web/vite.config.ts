927aea2f9cc050b7231782fba9010c08e2e06b1e
Nikil Kuruvilla <nikilok@gmail.com>
2026-05-21 19:53:13 +0100

    feat: revert the cache invalidation (#120)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -40,6 +40,11 @@ const config = defineConfig({
               "frame-ancestors 'none'; base-uri 'self'; object-src 'none'; form-action 'self'; upgrade-insecure-requests",
           },
         },
+        '/company/**': {
+          headers: {
+            'Cache-Control': 's-maxage=2592000, stale-while-revalidate=604800',
+          },
+        },
         '/api/tiles/**': {
           headers: {
             'Cache-Control': 's-maxage=31536000, stale-while-revalidate=86400',
